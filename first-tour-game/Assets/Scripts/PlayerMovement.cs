2908e13916863e1a5950f48b270738920b0b0d28
MappetProd <fwexample821@gmail.com>
2023-04-06 00:16:36 +0300

    basic movement implemented

---
+++ first-tour-game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private Animator animator;
+    Vector2 movement;
+    
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    //обработка ввода
+    void Update()
+    {
+        ProcessInputForMovement();
+    }
+
+    //обработка движения
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    private void ProcessInputForMovement()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+}
