a32cb999b7c5cbce6ccc96391a0408469ff9b574
MappetProd <fwexample821@gmail.com>
2023-04-09 22:14:30 +0300

    ricochet added

--- first-tour-game/Assets/Scripts/BulletBehavior.cs
+++ first-tour-game/Assets/Scripts/BulletBehavior.cs
@@ -5,10 +5,24 @@ using UnityEngine;
 public class BulletBehavior : MonoBehaviour
 {
     [SerializeField] int damage;
+    private Rigidbody2D rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("Collistion for bullit is " + collision.transform.name);
+
+        if (gameObject.CompareTag("Bullet") && collision.gameObject.CompareTag("blockingLayer"))
+        {
+            Vector2 wallNormal = collision.contacts[0].normal;
+            Vector2 dir = Vector2.Reflect(rb.velocity, wallNormal).normalized;
+            rb.velocity = dir * 10f;
+            StartCoroutine(DestroyAfterTime());
+            return;
+        }
 
         //player hit enemies
         if (gameObject.tag == "Bullet" && (collision.collider.tag == "Enemy" || collision.collider.tag == "ArtilleryEnemy"))
@@ -28,6 +42,12 @@ public class BulletBehavior : MonoBehaviour
             Destroy(collision.gameObject);
         }
 
+        Destroy(gameObject);
+    }
+
+    IEnumerator DestroyAfterTime()
+    {
+        yield return new WaitForSeconds(1f);
         Destroy(gameObject);
     }
 }
