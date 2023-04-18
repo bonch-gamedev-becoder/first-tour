618f10f61e31685258bca41ce48b11af9b80bc0d
MappetProd <fwexample821@gmail.com>
2023-04-18 15:52:36 +0300

    animations + template level editor

--- first-tour-game/Assets/LevelEditor/LE Scripts/TilemapInitializer.cs
+++ first-tour-game/Assets/LevelEditor/LE Scripts/TilemapInitializer.cs
@@ -19,6 +19,25 @@ public class TilemapInitializer : Singleton<TilemapInitializer>
             GameObject obj = new GameObject("Tilemap_" + category.name);
             Tilemap map = obj.AddComponent<Tilemap>();
             TilemapRenderer tr = obj.AddComponent<TilemapRenderer>();
+            if (category.name == "Wall")
+            {
+                obj.AddComponent<TilemapCollider2D>();
+                obj.tag = "blockingLayer";
+                obj.layer = 7;
+            }
+            else if (category.name == "WallBreakable")
+            {
+                obj.AddComponent<TilemapCollider2D>();
+                obj.tag = "blockingLayerBreakable";
+                obj.layer = 12;
+            }
+            else if (category.name == "FloorBlock")
+            {
+                TilemapCollider2D col = obj.AddComponent<TilemapCollider2D>();
+                col.isTrigger = true;
+                obj.tag = "BoostBlock";
+                obj.layer = 11;
+            }
 
             obj.transform.SetParent(grid);
 
